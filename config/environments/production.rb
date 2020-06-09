3d83687f7317e243fcd9bad67dc1e99841a77821
Armand Guillemot <armand.guillemot.56@gmail.com>
2020-06-09 17:12:50 +0200

    this is a new comit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
-  config.action_mailer.default_url_options = { host: 'lavidaloca.herokuapp.com',port: 80 }
+  config.action_mailer.default_url_options = { host: 'community-lite.herokuapp.com',port: 80 }
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
